fa36071e6d860604e3876f3bb081f1ced18a461e
Artyom Bashkatov <ar.bashkatov@saychas.ru>
2023-09-04 17:43:04 +0300

    Added Project to github

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Артём Башкатов on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
